65483c79ed19f10640af7c9990d1d4090f1d7344
sagarphate <sagarsphate@gmail.com>
2026-02-27 19:52:56 +0530

    changes made in file

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -6,11 +6,11 @@ For numbers that are multiples of both 3 and 5, print "FizzBuzz."
  */
 
 for(let i=1;i<=100;i++){
-    if(i%3==0 && i%5==0){
+    if(i%3 === 0 && i%5 === 0){
         console.log("FizzBuzz")
-    }else if(i%3==0){
+    }else if(i%3=== 0){
         console.log("Fizz")
-    }else if(i%5==0){
+    }else if(i%5=== 0){
         console.log("Buzz")
     }else{
         console.log(i)
